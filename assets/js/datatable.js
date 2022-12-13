a8d67ad432a5f5cb71cda202aac7f929ada733ab
Antoine Stoecklin <astoeck@hotmail.fr>
2022-12-13 18:03:04 +0100

    1st push

---
+++ assets/js/datatable.js
@@ -0,0 +1,11 @@
+$(function(e) {
+	$('#example').DataTable({
+		"dom": 'rtip'
+	  });
+	$('#example2').DataTable({
+		"dom": 'rtip'
+	  });
+} );
+$(function(e) {
+	$('.data-table-example').DataTable();
+} );
